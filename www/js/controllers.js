d910964423d12b98616a049dc662db8408df1bea
Sam Schwartz <obct537@gmail.com>
2015-05-07 20:42:00 -0500

    Added wiki create/edit/delete

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['starter.calcs', 'firebase'])
+angular.module('starter.controllers', ['starter.calcs', 'firebase', 'ionic'])
 
 .controller('DashCtrl', function($scope) {})
 
@@ -13,17 +13,74 @@ angular.module('starter.controllers', ['starter.calcs', 'firebase'])
   $scope.planet = Planets.get($stateParams.planetId);
 })
 
-.controller('WikiCtrl', function($scope, $firebaseArray) {
+.controller('WikiCtrl', function($scope, $firebaseArray, $ionicPopup) {
   var fb = new Firebase("https://kspmobile.firebaseio.com/wikiPages");
 
   $scope.pages = $firebaseArray(fb);
   $scope.myPage = null;
   $scope.bodyText = "";
 
+  $scope.updateEditor = function() {
+      var element = document.getElementById("pageText");
+      element.style.height = element.scrollHeight + "px";
+  };
 
-  $scope.setText = function(page) {
-      $scope.myPage = this.myPage;
-      $scope.bodyText = $scope.myPage.text;
+  $scope.savePages = function(page) {
+    $scope.pages.$save(page).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  $scope.removePage = function(page) {
+
+    var myPopup = $ionicPopup.show({
+      title: 'Are you sure?',
+      subTitle: 'really?',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Delete</b>',
+          type: 'button-assertive',
+          onTap: function(e) {
+            $scope.pages.$remove($scope.pages.$getRecord(page.$id)).catch(function(error) {
+              console.log(error);
+            });
+          }
+        }
+      ]
+    });
+  }
+
+  $scope.showPopup = function() {
+    $scope.data = {}
+
+    var template = '<label class="item item-input">' +
+                      '<span class="input-label">Title</span>' +
+                      '<input ng-model="data.title" type="text">' +
+                    '</label>';
+
+    var myPopup = $ionicPopup.show({
+      template: template,
+      title: 'Create Wiki page',
+      subTitle: 'I\'m not sure why...',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Save</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.data.title) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              $scope.pages.$add({ name: $scope.data.title, text: 'Blank Page...'});
+            }
+          }
+        }
+      ]
+    });
   }
 })
 
